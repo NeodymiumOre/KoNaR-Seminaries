f8476e042929b9eee080e3960df7b4d7f65de5b0
NeodymiumOre <maciejbarna@gmail.com>
2022-05-05 17:24:03 +0200

    Quick fix in SerialCommunication.py (it didn't worked)

--- vision-control/VisionPart/SerialCommunication.py
+++ vision-control/VisionPart/SerialCommunication.py
@@ -4,11 +4,11 @@ import serial
 import time
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
 def arduinoSendValue(x):
-    arduino.write(x)
+    arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline()
     print(data)
 
 while True:
     num = input("Enter a number between 0-100: ")
-    sendValue(num)
+    arduinoSendValue(num)
